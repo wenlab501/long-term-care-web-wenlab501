d60d611de47bb039e34bcffda09bc52fea76e1ac
wenlab501 <wenlab501@github.com>
2025-09-02 21:00:38 +0800

    Update project configuration for long-term-care-web-wenlab501 deployment

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
-  publicPath: '/long-term-care-web-taichung/',
+  publicPath: '/long-term-care-web-wenlab501/',
   transpileDependencies: true,
   devServer: {
     port: 8080,
